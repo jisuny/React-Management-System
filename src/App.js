f7771b84a6c423c8126b0ab46f10290a73172f78
[jiseon] <[yn5392@naver.com]>
2023-07-26 15:34:19 +0900

    css+data update

--- src/App.js
+++ src/App.js
@@ -10,25 +10,25 @@ import TableCell from '@mui/material/TableCell';
 const customer =[
   {
   'id':1,
-  'image': 'https://placeimg.com/64/64/any',
-  'name': '홍길동',
+  'image': 'http://jsyoon.dothome.co.kr/no_image.jpg',
+  'name': '윤지선',
   'birthday': '950211',
   'gender': '여',
   'job': '직장인'
   },
   {
     'id':2,
-    'image': 'https://placeimg.com/64/64/any',
-    'name': '홍홍',
+    'image': 'http://jsyoon.dothome.co.kr/no_image.jpg',
+    'name': '김김',
     'birthday': '950211',
-    'gender': '나',
+    'gender': '남',
     'job': '학생'
   },
   {
     'id':3,
-    'image': 'https://placeimg.com/64/64/any',
-    'name': '호어이',
-    'birthday': '12565',
+    'image': 'http://jsyoon.dothome.co.kr/no_image.jpg',
+    'name': '윤윤',
+    'birthday': '961222',
     'gender': '여',
     'job': '학생'
   }    
