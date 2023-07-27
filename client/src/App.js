ec46dfbaaf2a5b76b88f3b3e630f1e2d384f3a60
[jiseon] <[yn5392@naver.com]>
2023-07-27 11:44:42 +0900

    0727 신규

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React, {Component} from 'react';
 import './App.css';
 import Customer from './component/Customer';
 import Paper from '@mui/material/Paper';
@@ -7,66 +8,46 @@ import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 
-const customer =[
-  {
-  'id':1,
-  'image': 'http://jsyoon.dothome.co.kr/no_image.jpg',
-  'name': '윤지선',
-  'birthday': '950211',
-  'gender': '여',
-  'job': '직장인'
-  },
-  {
-    'id':2,
-    'image': 'http://jsyoon.dothome.co.kr/no_image.jpg',
-    'name': '김김',
-    'birthday': '950211',
-    'gender': '남',
-    'job': '학생'
-  },
-  {
-    'id':3,
-    'image': 'http://jsyoon.dothome.co.kr/no_image.jpg',
-    'name': '윤윤',
-    'birthday': '961222',
-    'gender': '여',
-    'job': '학생'
-  }    
-]
+class App extends Component {
+  state ={
+    customers: ""
+  }
 
-function App() {
-  return (
-    <Paper className='paper'>
-      <Table className='table'>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>프로필 사진</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {customer.map(c=> {
-            return(
-                <Customer 
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-              
-            )
-          })}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+  componentDidMount(){
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err))
+  }
+
+  callApi = async () => {
+    const response = await fetch('api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  render(){
+    return (
+      <Paper className='paper'>
+        <Table className='table'>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>프로필 사진</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customer ? this.state.customerthis.state.customer.map(c=> {
+              return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>)
+            }):""}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res)=> {
-    res.send({message:'Hello Express!'});
+app.get('/api/customers', (req, res)=> {
+    res.send([
+        {
+        'id':1,
+        'image': 'http://jsyoon.dothome.co.kr/no_image.jpg',
+        'name': '윤지선',
+        'birthday': '950211',
+        'gender': '여',
+        'job': '직장인'
+        },
+        {
+            'id':2,
+            'image': 'http://jsyoon.dothome.co.kr/no_image.jpg',
+            'name': '김김',
+            'birthday': '950211',
+            'gender': '남',
+            'job': '학생'
+        },
+        {
+            'id':3,
+            'image': 'http://jsyoon.dothome.co.kr/no_image.jpg',
+            'name': '윤윤',
+            'birthday': '961222',
+            'gender': '여',
+            'job': '학생'
+        }    
+    ]);
 })
 
 app.listen(port, ()=> console.log(`listening on port ${port}`));
